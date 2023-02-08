e6f142d489148e11332b5fcbc7fa31bc1b66f3ff
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2023-02-08 11:37:44 -0300

    User update

--- src/main/java/com/kathfss/workshopmongo/service/UserService.java
+++ src/main/java/com/kathfss/workshopmongo/service/UserService.java
@@ -26,12 +26,24 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-												
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
